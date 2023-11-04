b59aac07363f35b5cacd2a9c800ae469f6eacde8
lucianthomaz <lucianthomaz@hotmail.com>
2023-11-04 17:03:56 -0300

    Rename usecase from VisualizeAlert to GetAlertsByUser

--- application/src/main/java/com/lucianthomaz/alpr/alprintegration/application/configuration/InteractorConfiguration.java
+++ application/src/main/java/com/lucianthomaz/alpr/alprintegration/application/configuration/InteractorConfiguration.java
@@ -5,7 +5,7 @@ import com.lucianthomaz.alpr.alprintegration.domain.repositoryInterface.*;
 import com.lucianthomaz.alpr.alprintegration.interactor.alert.create.AlertCreationInteractor;
 import com.lucianthomaz.alpr.alprintegration.interactor.alert.create.SendToUserInteractor;
 import com.lucianthomaz.alpr.alprintegration.interactor.alert.useralertaction.UserAlertActionInteractor;
-import com.lucianthomaz.alpr.alprintegration.interactor.alert.visualize.VisualizeAlertsInteractor;
+import com.lucianthomaz.alpr.alprintegration.interactor.alert.getalertsbyuser.GetAlertsByUserInteractor;
 import com.lucianthomaz.alpr.alprintegration.interactor.alerttype.AlertTypeCreationInteractor;
 import com.lucianthomaz.alpr.alprintegration.interactor.location.LocationCreationInteractor;
 import com.lucianthomaz.alpr.alprintegration.interactor.user.UserCreationInteractor;
@@ -13,7 +13,7 @@ import com.lucianthomaz.alpr.alprintegration.interactor.vehicle.VehicleCreationI
 import com.lucianthomaz.alpr.alprintegration.usecase.alert.create.AlertCreationUseCase;
 import com.lucianthomaz.alpr.alprintegration.usecase.alert.sendtouser.SendToUserUseCase;
 import com.lucianthomaz.alpr.alprintegration.usecase.alert.useralertaction.UserAlertActionUseCase;
-import com.lucianthomaz.alpr.alprintegration.usecase.alert.visualize.VisualizeAlertsUseCase;
+import com.lucianthomaz.alpr.alprintegration.usecase.alert.getalertsbyuser.GetAlertsByUserUseCase;
 import com.lucianthomaz.alpr.alprintegration.usecase.alerttype.create.AlertTypeCreationUseCase;
 import com.lucianthomaz.alpr.alprintegration.usecase.location.create.LocationCreationUseCase;
 import com.lucianthomaz.alpr.alprintegration.usecase.user.create.UserCreationUseCase;
@@ -51,8 +51,8 @@ public class InteractorConfiguration {
 
     @Bean
     @Autowired
-    SendToUserUseCase sendToUserUseCase(UserAlertRepository userAlertRepository) {
-        return new SendToUserInteractor(userAlertRepository);
+    SendToUserUseCase sendToUserUseCase(UserAlertRepository userAlertRepository, AlertRepository alertRepository) {
+        return new SendToUserInteractor(userAlertRepository, alertRepository);
     }
 
     @Bean
@@ -64,8 +64,8 @@ public class InteractorConfiguration {
 
     @Bean
     @Autowired
-    VisualizeAlertsUseCase visualizeAlertsUseCase(AlertRepository alertRepository, ObjectMapper objectMapper) {
-        return new VisualizeAlertsInteractor(alertRepository, objectMapper);
+    GetAlertsByUserUseCase getAlertsByUserUseCase(AlertRepository alertRepository, ObjectMapper objectMapper) {
+        return new GetAlertsByUserInteractor(alertRepository, objectMapper);
     }
 
     @Bean
