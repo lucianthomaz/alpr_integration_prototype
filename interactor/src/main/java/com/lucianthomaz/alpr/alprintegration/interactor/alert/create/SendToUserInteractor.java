31eb7ca21ab4e78745c9fc6167035243ca13e4e3
lucianthomaz <lucianthomaz@hotmail.com>
2023-11-04 17:04:37 -0300

    Update Alert status

--- interactor/src/main/java/com/lucianthomaz/alpr/alprintegration/interactor/alert/create/SendToUserInteractor.java
+++ interactor/src/main/java/com/lucianthomaz/alpr/alprintegration/interactor/alert/create/SendToUserInteractor.java
@@ -1,20 +1,30 @@
 package com.lucianthomaz.alpr.alprintegration.interactor.alert.create;
 
+import com.lucianthomaz.alpr.alprintegration.domain.Alert;
+import com.lucianthomaz.alpr.alprintegration.domain.StatusEnum;
 import com.lucianthomaz.alpr.alprintegration.domain.UserAlert;
+import com.lucianthomaz.alpr.alprintegration.domain.repositoryInterface.AlertRepository;
 import com.lucianthomaz.alpr.alprintegration.domain.repositoryInterface.UserAlertRepository;
 import com.lucianthomaz.alpr.alprintegration.usecase.alert.sendtouser.SendToUserRequest;
 import com.lucianthomaz.alpr.alprintegration.usecase.alert.sendtouser.SendToUserUseCase;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 public class SendToUserInteractor implements SendToUserUseCase {
 
     private final UserAlertRepository repository;
+    private final AlertRepository alertRepository;
 
     @Override
     public List<UserAlert> execute(SendToUserRequest request) {
+        Optional<Alert> alert = alertRepository.getDetails(request.alertId());
+        alert.ifPresent(x -> {
+            x.setStatus(StatusEnum.USERS_NOTIFIED.name());
+            alertRepository.save(x);
+        });
         return repository.sendAlertToUsers(request.alertId(), request.usersId());
     }
 }
--- interactor/src/main/java/com/lucianthomaz/alpr/alprintegration/interactor/alert/useralertaction/UserAlertActionInteractor.java
+++ interactor/src/main/java/com/lucianthomaz/alpr/alprintegration/interactor/alert/useralertaction/UserAlertActionInteractor.java
@@ -2,6 +2,7 @@ package com.lucianthomaz.alpr.alprintegration.interactor.alert.useralertaction;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lucianthomaz.alpr.alprintegration.domain.Alert;
+import com.lucianthomaz.alpr.alprintegration.domain.StatusEnum;
 import com.lucianthomaz.alpr.alprintegration.domain.repositoryInterface.AlertRepository;
 import com.lucianthomaz.alpr.alprintegration.domain.repositoryInterface.UserAlertRepository;
 import com.lucianthomaz.alpr.alprintegration.usecase.alert.shared.UserAlertResponse;
@@ -22,6 +23,10 @@ public class UserAlertActionInteractor implements UserAlertActionUseCase {
     public void execute(UserAlertActionRequest request, UserAlertActionResponder responder) {
         userAlertRepository.userAlertAction(request.alertId(), request.userId(), request.accepted());
         Optional<Alert> alert = alertRepository.getDetails(request.alertId());
+        if (request.accepted()) {
+            alert.ifPresent(x -> x.setStatus(StatusEnum.ACCEPTED.name()));
+            alertRepository.save(alert.get());
+        }
         UserAlertResponse response = alert.map(x -> {
             UserAlertResponse alertResponse = objectMapper.convertValue(x, UserAlertResponse.class);
             alertResponse.setAccepted(request.accepted());
